ead2ffa178f254f0fe5492c14efa1d1d49327c79
ZCooldudeguy <731256@student.dusd.net>
2026-04-13 15:28:30 -0700

    e
    e

---
+++ P7FinnellUnit4/Assets/Scrips/RotateCamera.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class RotateCamera : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+    }
+}
